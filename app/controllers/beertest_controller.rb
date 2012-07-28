12b60c6ff07fceb8da9bb5ec36e3f76c43e1c43c
rosslazer <rosslazer@gmail.com>
2012-07-28 13:38:24 -0400

    Got search working

--- app/controllers/beertest_controller.rb
+++ app/controllers/beertest_controller.rb
@@ -1,8 +1,18 @@
 class BeertestController < ApplicationController
   def index
 
-  	@results = BreweryDb2.search(:q => 'hoegarden')
+  	beersearch = "coors light"
+  	@match = ""
 
+  	results = BreweryDb2.search(:q => beersearch)    
+
+  	results.each do |beer|
+  		if beer.name.casecmp(beersearch)
+  			@match = beer.name
+  		end
+  	end
 
   end
 end
+
+
