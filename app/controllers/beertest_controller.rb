2f8b442796b2fcf1d97d4f861759a822a5013bab
Doug Crescenzi <doug@douglascrescenzi.com>
2012-07-28 15:10:10 -0400

    modified controller

--- app/controllers/beertest_controller.rb
+++ app/controllers/beertest_controller.rb
@@ -10,8 +10,22 @@ class BeertestController < ApplicationController
   		if beer.name.casecmp(beersearch)
   			@match = beer.name
   		end
-
   	end
+
+    #twilio stuff
+
+    require 'rubygems'
+    require 'twilio-ruby'
+
+    @account_sid = 'AC0626c6d8dc0a4551b159161c5ca7ced2'
+    @auth_token = 'd27c1fd426321b9306b58df6dd3a24aa'
+
+    # set up a client to talk to the Twilio REST API
+    @client = Twilio::REST::Client.new(@account_sid, @auth_token)
+
+    message_body = params["Body"]
+
+    puts "hello!"
 
 
   end
