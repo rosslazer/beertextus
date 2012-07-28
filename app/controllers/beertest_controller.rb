59284b97cbfe00aff30917c3dbac88a92a97fec9
rosslazer <rosslazer@gmail.com>
2012-07-28 17:20:16 -0400

    We are close

--- app/controllers/beertest_controller.rb
+++ app/controllers/beertest_controller.rb
@@ -1,35 +1,11 @@
 class BeertestController < ApplicationController
   def index
 
-	###################################
-	######## SEARCH VARIABLES #########
-	arrayLimit = 4 # Max length of list
-	minChars = 3 # Min characters user must enter into search
-	###################################
-
-  	
-	beersearch = "coors" #FOR DEBUGGING
-  	@match = []
-
-  	results = BreweryDb2.search(:q => beersearch)    
-
-  	if beersearch.length >= minChars
-		results.each do |beer|
-			# regex match
-  			if beer.name.downcase.include? beersearch.downcase
-				@match << beer.name
-			end
-  		end
-	else
-		@match << "Please use at least #{minChars} characters"
-	end
-
-	@match = @match.take(arrayLimit)
-
-    #twilio stuff
-
     require 'rubygems'
     require 'twilio-ruby'
+     #twilio stuff
+
+   
 
     @account_sid = 'AC0626c6d8dc0a4551b159161c5ca7ced2'
     @auth_token = 'd27c1fd426321b9306b58df6dd3a24aa'
@@ -37,9 +13,49 @@ class BeertestController < ApplicationController
     # set up a client to talk to the Twilio REST API
     @client = Twilio::REST::Client.new(@account_sid, @auth_token)
 
-    message_body = params["Body"]
+    #beersearch = params["Body"]
+    beersearch = "Coors Light"
 
-    puts "hello! #{message_body}"
+
+
+	###################################
+	######## SEARCH VARIABLES #########
+	arrayLimit = 4 # Max length of list
+	minChars = 3 # Min characters user must enter into search
+	###################################
+
+  	
+  	@match = []
+    @final =""
+
+  	results = BreweryDb2.search(:q => beersearch)    
+
+  	if beersearch.length >= minChars
+		results.each do |beer|
+			# regex match
+  			if beer.name.downcase.include? beersearch.downcase
+				@match << beer
+			end
+  		end
+	else
+		@match << "Please use at least #{minChars} characters"
+    @final = @match
+	end
+
+	@match = @match.take(arrayLimit)
+
+
+  if @match.length ==1
+    @final = "Name:#{@match[0].name}" + " " + "Description:#{@match[0].description}"
+  else
+    @match.each do |beer|
+      @final += beer.name + ", "
+    end
+  end
+
+  puts "hello! #{@match}"
+
+   
 
   end
 end
