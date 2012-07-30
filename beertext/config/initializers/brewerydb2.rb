b0728b36de67aa543d66a9e2c74d8f6c8761aef6
root <root@carter-VirtualBox.(none)>
2012-07-30 18:25:05 -0400

    fixed tabbing and comments

--- beertext/config/initializers/brewerydb2.rb
+++
@@ -1,3 +0,0 @@
-BreweryDb2.configure do |config|
-  config.apikey = '9694c7382740c1afd4f0dff20cf58da3'
-end
--- beertext/test/unit/helpers/beertest_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class BeertestHelperTest < ActionView::TestCase
-end
