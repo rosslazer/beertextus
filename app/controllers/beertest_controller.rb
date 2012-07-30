5c4b8fccc1a46b50cb1bb391dbd0357164762ef2
root <root@carter-VirtualBox.(none)>
2012-07-30 18:18:22 -0400

    Fixed tabbing and comments for beer controller

--- app/controllers/beertest_controller.rb
+++ app/controllers/beertest_controller.rb
@@ -1,72 +1,66 @@
 class BeertestController < ApplicationController
   def index
     
+    # import gems
     require 'rubygems'
     require 'twilio-ruby'
 
+    # auth for Twilio
     @account_sid = 'AC0626c6d8dc0a4551b159161c5ca7ced2'
     @auth_token = 'd27c1fd426321b9306b58df6dd3a24aa'
 
     # set up a client to talk to the Twilio REST API
     @client = Twilio::REST::Client.new(@account_sid, @auth_token)
-  
+
+    # get sender's search and number
     beersearch = params["Body"]
     from_number = params["From"]
 
-  	@match = ""
-
-	###################################
-	######## SEARCH VARIABLES #########
-	arrayLimit = 4 # Max length of list
-	minChars = 3 # Min characters user must enter into search
-	###################################
+    # search variables
+    arrayLimit = 4 # max length of beer list
+    minChars = 3 # min characters user must enter for search
   	
-  	@match = []
+    # used to store and format search results
+    @match = []
     @final =""
 
-  	results = BreweryDb2.search(:q => beersearch)
-  
+    # query BreweryDB for any matches
+    results = BreweryDb2.search(:q => beersearch)
 
-  if beersearch.length >= minChars
-		# Regex Search
-		results.each do |beer|
-  			if beer.name.downcase.include? beersearch.downcase
-				@match << beer
-			  end
-  	  end
-		# If there is an exact match, only return that beer
-		results.each do |beer|
-			if beer.name.downcase == beersearch.downcase
-				@match = [beer]
-			end
-	 end
+    # improve search results
+    if beersearch.length >= minChars
+      # regex filter
+      results.each do |beer|
+        if beer.name.downcase.include? beersearch.downcase
+          @match << beer
+        end
+      end
+      # if there is an exact match, only return that beer
+      results.each do |beer|
+        if beer.name.downcase == beersearch.downcase
+          @match = [beer]
+	end
+      end
+    # return error if user doesn't enter enough search characters
+    else
+      @match << "Please use at least #{minChars} characters"
+      @final = @match
+    end
 
-	else
-		@match << "Please use at least #{minChars} characters"
-    @final = @match
+    # trim search results to arrayLimit
+    @match = @match.take(arrayLimit)
 
- end
-
-
-   
-
-	@match = @match.take(arrayLimit)
-
-  if @match.length ==1
-    @final = "Name: #{@match[0].name}" + " " + "Description: #{@match[0].description}" + " " + "ABV: #{@match[0].abv}%"
-  elsif  @match == nil || @match == [] || @match == {} || @match == ""
-      @final = "Sorry, no matches found. Please check your spelling."   
-  else
-    @match.each do |beer|
+    # if only one beer matched search (or error message), return info on beer
+    if @match.length == 1
+      @final = "Name: #{@match[0].name}" + " " + "Description: #{@match[0].description}"
+    # otherwise return list of beers
+    else
+      @match.each do |beer|
       @final += beer.name + ", "
     end
   end
 
-
-  ################################################################
-  ### Handling instances where responses are more than one SMS ###
-  ################################################################
-
+  #cut message into multiple sms and send
   count = @final.length / 160.0
 
   while count > 0
@@ -76,14 +70,9 @@ class BeertestController < ApplicationController
     :body => " #{@final.first(160)}"
     )
 
-    @final = @final[160,@final.length-1]
+    @final = @final[161,@final.length-1]
     count = count - 1
-
-    sleep 1
   end
   
   end #method end
 end #controller end
-
-
-
